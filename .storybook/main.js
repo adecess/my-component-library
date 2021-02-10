970ae8d4dc2d06c7d61fa9d0a2805104842ac089
adecess <axel.saliniere@protonmail.com>
2021-02-09 22:15:47 -0400

    add sb actions

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ["../src/**/*.stories.(js|mdx)"],
   addons: [
     "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
     "@storybook/addon-docs",
     "@storybook/addon-contexts/register",
     "@storybook/addon-backgrounds/register",
